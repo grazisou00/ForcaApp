96412001e0ad54da2eafb4d8886296bd6b238c2c
Graziela Souza <118382634+grazisou00@users.noreply.github.com>
2024-05-02 14:56:46 -0300

    Update hangman_screen.dart

--- jogo_forca/lib/hangman/hangman_screen.dart
+++ jogo_forca/lib/hangman/hangman_screen.dart
@@ -1,6 +1,8 @@
+
 import 'package:flutter/material.dart';
 import 'hangman_painter.dart';
 import 'dart:math';
+import 'body_part.dart';
 
 class HangmanScreen extends StatefulWidget {
   @override
@@ -8,29 +10,59 @@ class HangmanScreen extends StatefulWidget {
 }
 
 class _HangmanScreenState extends State<HangmanScreen> {
-  final List<String> _words = ['FLUTTER', 'DESENVOLVIMENTO', 'PROGRAMAÇÃO']; // Lista de palavras
-  final List<String> _hints = ['Framework para UI', 'Construção de software', 'Escrever código']; // Lista de dicas
+  final List<String> _words = ['FLUTTER', 'DESENVOLVIMENTO', 'PROGRAMAÇÃO', 'ALGORITMO', 'FUNÇÃO', 'LOOP', 'ARRAY', 'COMPILADOR', 'DEBUGAR', 'RECURSIVIDADE'];
+  final List<String> _hints = ['Framework para UI', 'Construção de software', 'Escrever código','Conjunto de passos sequenciais para resolver um problema', 'bloco de código reutilizável que realiza uma tarefa específica', 'Uma estrutura de controle que repete um bloco de código até que uma condição seja atendida', 'estrutura de dados que armazena uma coleção de elementos do mesmo tipo', 'Programa que traduz código-fonte em linguagem de programação para código de máquina', 'Processo de identificar e corrigir erros em um programa', 'conceito em programação onde uma função chama a si mesma repetidamente até atingir uma condição de parada' ];
   String _selectedWord = '';
-  String _hint = '';
   String _maskedWord = '';
-  int _tries = 6; // Número de tentativas permitidas
+  int _tries = 6;
   List<String> _guessedLetters = [];
   TextEditingController _wordController = TextEditingController();
   bool _customWord = false;
   bool _gameOver = false;
+  List<BodyPart> _bodyParts = [];
+  List<int> _bodyPartsShown = [];
+  bool _showHint = false;
+
+  List<Widget> _buildAlphabetButtons() {
+    return List.generate(26, (index) {
+      final letter = String.fromCharCode('A'.codeUnitAt(0) + index);
+      return _buildButton(letter);
+    });
+  }
+
+  List<Widget> _buildSpecialCharacterButtons(List<String> characters) {
+    return characters.map((char) => _buildButton(char)).toList();
+  }
+
+  final List<BodyPart> _bodyPartsList = [
+    BodyPart.head,
+    BodyPart.body,
+    BodyPart.leftArm,
+    BodyPart.rightArm,
+    BodyPart.leftLeg,
+    BodyPart.rightLeg,
+  ];
 
   @override
   void initState() {
     super.initState();
     _pickRandomWord();
+    _bodyPartsShown.clear();
   }
 
   void _pickRandomWord() {
     final random = Random();
     final index = random.nextInt(_words.length);
     _selectedWord = _words[index];
-    _hint = _hints[index];
-    _maskedWord = _selectedWord.replaceAll(RegExp(r'\w'), '_');
+    _maskedWord = '';
+    for (int i = 0; i < _selectedWord.length; i++) {
+      if (_selectedWord[i].toUpperCase().contains(RegExp(r'[A-ZÇÃÕÊ]'))) {
+        _maskedWord += '_';
+      } else {
+        _maskedWord += _selectedWord[i];
+      }
+    }
+
     _customWord = false;
     _gameOver = false;
   }
@@ -43,47 +75,51 @@ class _HangmanScreenState extends State<HangmanScreen> {
           : Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Dica: $_hint'),
-                SizedBox(height: 20),
+                if (_showHint) _buildHint(),
+                const SizedBox(height: 20),
                 _buildHangman(),
-                SizedBox(height: 20),
-                Text(_maskedWord, style: TextStyle(fontSize: 24)),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
+                Text(_maskedWord, style: const TextStyle(fontSize: 24)),
+                const SizedBox(height: 20),
                 if (!_customWord) _buildKeyboard(),
                 if (_customWord) _buildWordInput(),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Text('Tentativas restantes: $_tries'),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () => _giveUp(),
-                      child: Text('Desistir'),
-                    ),
-                    SizedBox(width: 20),
-                    if (_tries == 0) _buildRevealButton(),
-                  ],
-                ),
+                const SizedBox(height: 20),
+                if (_tries == 0) _buildRevealButton(),
               ],
             ),
     );
   }
 
   Widget _buildGameOverScreen() {
+    String message = _tries == 0 ? 'Você perdeu!' : _gameOver ? 'Você venceu!' : 'Você desistiu!';
+    if (_gameOver) {
+      _maskedWord = message; 
+    }
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
-          _tries == 0 ? 'Você perdeu!' : 'Você venceu!',
-          style: TextStyle(fontSize: 24),
+          message,
+          style: const TextStyle(fontSize: 24),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         ElevatedButton(
           onPressed: () => _resetGame(),
-          child: Text('Jogar Novamente'),
+          child: const Text('Jogar Novamente'),
         ),
       ],
+    );
+  }
+
+  Widget _buildButton(String text) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: ElevatedButton(
+        onPressed: _guessedLetters.contains(text) ? null : () => _guessLetter(text),
+        child: Text(text),
+      ),
     );
   }
 
@@ -92,7 +128,7 @@ class _HangmanScreenState extends State<HangmanScreen> {
       width: 200,
       height: 200,
       child: CustomPaint(
-        painter: HangmanPainter(_tries),
+        painter: HangmanPainter(_bodyParts),
       ),
     );
   }
@@ -102,16 +138,10 @@ class _HangmanScreenState extends State<HangmanScreen> {
       alignment: WrapAlignment.center,
       spacing: 8.0,
       runSpacing: 8.0,
-      children: List.generate(26, (index) {
-        final letter = String.fromCharCode('A'.codeUnitAt(0) + index);
-        return Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: ElevatedButton(
-            onPressed: _guessedLetters.contains(letter) ? null : () => _guessLetter(letter),
-            child: Text(letter),
-          ),
-        );
-      }),
+      children: [
+        ..._buildAlphabetButtons(),
+        ..._buildSpecialCharacterButtons(['Ç', 'Ã', 'Õ', 'Ê']), // Adiciona botões para caracteres especiais
+      ],
     );
   }
 
@@ -120,7 +150,14 @@ class _HangmanScreenState extends State<HangmanScreen> {
       onPressed: () {
         _showDialog('A palavra era:', _selectedWord);
       },
-      child: Text('Revelar Palavra'),
+      child: const Text('Revelar Palavra'),
+    );
+  }
+
+  Widget _buildHint() {
+    return Text(
+      'Dica: ${_hints[_words.indexOf(_selectedWord)]}',
+      style: const TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
     );
   }
 
@@ -129,22 +166,21 @@ class _HangmanScreenState extends State<HangmanScreen> {
       children: [
         TextField(
           controller: _wordController,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             labelText: 'Insira a palavra secreta',
           ),
         ),
-        SizedBox(height: 20),
+        const SizedBox(height: 20),
         ElevatedButton(
           onPressed: () {
             if (_wordController.text.isNotEmpty) {
               _selectedWord = _wordController.text.toUpperCase();
-              _hint = 'Palavra Personalizada';
               _maskedWord = _selectedWord.replaceAll(RegExp(r'\w'), '_');
               _customWord = true;
               setState(() {});
             }
           },
-          child: Text('Confirmar Palavra'),
+          child: const Text('Confirmar Palavra'),
         ),
       ],
     );
@@ -159,7 +195,14 @@ class _HangmanScreenState extends State<HangmanScreen> {
           _gameOver = true;
         }
       } else {
+        if (!_bodyPartsShown.contains(_tries)) {
+          _bodyParts.add(_bodyPartsList[6 - _tries]);
+          _bodyPartsShown.add(_tries);
+        }
         _tries--;
+        if (_tries == 2 && !_gameOver && !_showHint) {
+          _showHint = true;
+        }
         if (_tries == 0) {
           _gameOver = true;
         }
@@ -175,12 +218,6 @@ class _HangmanScreenState extends State<HangmanScreen> {
     }
   }
 
-  void _giveUp() {
-    setState(() {
-      _gameOver = true;
-    });
-  }
-
   void _showDialog(String title, String content) {
     showDialog(
       context: context,
@@ -194,7 +231,7 @@ class _HangmanScreenState extends State<HangmanScreen> {
                 Navigator.of(context).pop();
                 _resetGame();
               },
-              child: Text('Jogar Novamente'),
+              child: const Text('Jogar Novamente'),
             ),
           ],
         );
@@ -207,7 +244,10 @@ class _HangmanScreenState extends State<HangmanScreen> {
       _tries = 6;
       _guessedLetters.clear();
       _wordController.clear();
+      _bodyParts.clear();
+      _bodyPartsShown.clear();
       _pickRandomWord();
+      _showHint = false;
     });
   }
 
